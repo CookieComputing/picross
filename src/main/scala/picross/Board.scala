2a8b934a850d6c93da81157c1ef1b0542942f626
Kevin Hui <hui.k@northeastern.edu>
2021-12-22 10:07:03 -0500

    add board implementation and tests

--- src/main/scala/picross/Board.scala
+++ src/main/scala/picross/Board.scala
@@ -13,24 +13,45 @@ object Board {
   /**
    * Constructs a new board from the provided 2D list of tiles. Note that each list should
    * have equal length, otherwise this function will return None
+   *
    * @param tiles A 2D list of tiles, where the position of each tile in a list is
    * @return A fresh board containing tiles from the 2D list of tiles provided
    */
-  def newBoard(tiles: List[List[Tile]]): Option[Board] = ???
+  def newBoard(tiles: List[List[Tile]]): Option[Board] = tiles match
+    case Nil => None
+    case ts =>
+      val firstLen = ts.head.size
+      if firstLen == 0 || !ts.forall(_.size == firstLen)
+      then None else Some(ts.map(_.toIndexedSeq).toIndexedSeq)
 
   /**
-   * Retrieves the tile
-   * @param row
-   * @param col
-   * @param Board
-   * @return
+   * Creates a new tile
+   * @param colored Whether or not the tile is colored
+   * @return a freshly created tile
    */
-  def apply(row: Int, col: Int)(using Board): Option[Tile] = ???
+  def newTile(colored: Boolean): Tile = colored
+
+  /**
+   * Retrieves the tile located at (row, col)
+   *
+   * @param row   The requested row
+   * @param col   The requested col
+   * @param Board The board to be checked
+   * @return Some(tile) located at (row, col) for the given board, None if out of bounds
+   */
+  def tileAt(row: Int, col: Int)(using board: Board): Option[Tile] = {
+    def inBounds[A](tiles: IndexedSeq[A], index: Int): Boolean =
+      0 <= index && index < tiles.size
+
+    if inBounds(board, row) && inBounds(board(row), col) then
+      Some(board(row)(col)) else None
+  }
 
   /**
    * Determines if the provided tile should be colored or not
+   *
    * @param tile A tile to verify its coloring
    * @return Whether or not the tile is colored
    */
-  def colored(tile: Tile): Boolean = ???
+  def colored(tile: Tile): Boolean = tile
 }
---
+++ src/test/scala/BoardSpec.scala
@@ -0,0 +1,92 @@
+import BoardSpec.*
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Prop.passed
+import org.scalatest.FutureOutcome.failed
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import org.scalatest.prop.TableDrivenPropertyChecks.*
+import picross.Board
+import picross.Board.{Board, Tile}
+
+import scala.math
+
+class BoardSpec extends AnyPropSpec with ScalaCheckPropertyChecks {
+  property("a board should be non-empty") {
+    forAll(atLeastOneGapInTiles) { tiles =>
+      assert(Board.newBoard(tiles).isEmpty)
+    }
+  }
+
+  property("a board's rows should all be of equal length") {
+    forAll(differentSizedTiles) { tiles =>
+      assert(Board.newBoard(tiles).isEmpty)
+    }
+
+    forAll(sameSizedTiles) { tiles =>
+      assert(Board.newBoard(tiles).isDefined)
+
+      val firstRowSize = tiles.head.size
+      assert(tiles.forall(_.size == firstRowSize))
+    }
+  }
+
+  property("a board's tiles should be appropriately colored based on what was created") {
+    forAll(sameSizedTiles) { case (tiles: List[List[Tile]]) => {
+        val board = Board.newBoard(tiles)
+
+        board match
+          case None => failed()
+          case Some(b) =>
+            assert(tiles.map(_.zipWithIndex).zipWithIndex.forall(
+              (row, rowIndex) => row.forall(
+                (tile, colIndex) => Board.tileAt(rowIndex, colIndex)(using b) match
+                  case None => false
+                  case Some(boardTile) => Board.colored(boardTile) == Board.colored(tile))))
+      }
+    }
+  }
+}
+
+object BoardSpec {
+  private val tileRowGen: Int => Gen[List[Tile]] = rowSize =>
+    Gen.listOfN(rowSize, Gen.oneOf(Board.newTile(true), Board.newTile(false)))
+
+  val tileRowMaybeGap: Gen[List[Tile]] = for {
+    rowSize <- Gen.chooseNum(0, 100)
+    tiles <- tileRowGen(rowSize)
+  } yield tiles
+
+  val tileRowNoGap: Gen[List[Tile]] = for {
+    rowSize <- Gen.chooseNum(1, 100)
+    tiles <- tileRowGen(rowSize)
+  } yield tiles
+
+  val differentSizedTiles: Gen[List[List[Tile]]] = for {
+    initialRowSize <- Gen.chooseNum(1, 100)
+    differentRowSize <- Gen.chooseNum(1, 100) suchThat (n => n != initialRowSize)
+    firstTiles <- tileRowGen(initialRowSize)
+    secondTiles <- tileRowGen(differentRowSize)
+  } yield firstTiles :: List(secondTiles)
+
+  val sameSizedTiles: Gen[List[List[Tile]]] = for {
+    rowSize <- Gen.chooseNum(1, 100)
+    totalSize <- Gen.chooseNum(1, 100)
+    tiles <- Gen.listOfN(totalSize, tileRowGen(rowSize))
+  } yield tiles
+
+  val tilesWithPotentialGaps: Gen[List[List[Tile]]] =
+    for {
+      totalSize <- Gen.chooseNum(0, 100)
+      tiles <- Gen.listOfN(totalSize, tileRowMaybeGap)
+    } yield tiles
+
+  val atLeastOneGapInTiles: Gen[List[List[Tile]]] =
+    for {
+      tiles <- tilesWithPotentialGaps
+      randomIndex <- Gen.chooseNum(0, math.max(0, tiles.size - 1))
+    } yield
+      val (front, back) = tiles.splitAt(randomIndex)
+      front ++ List(List.empty[Tile]) ++ back
+
+}

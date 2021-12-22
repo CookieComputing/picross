9d5a28a5f3d50b0c2ed5264c9e9d9af2df6b40c0
Kevin Hui <hui.k@northeastern.edu>
2021-12-22 11:43:06 -0500

    add clue impl + table tests

--- src/main/scala/picross/Clue.scala
+++ src/main/scala/picross/Clue.scala
@@ -2,6 +2,8 @@ package picross
 
 import picross.Board.{Board, Tile}
 
+import scala.annotation.tailrec
+
 /**
  * Clues are a hint to the player that represent the number of tiles that are connected consecutively.
  * For instance, a "4" in a clue list indicates there are 4 tiles connected in a row. Elements that are separated
@@ -16,16 +18,27 @@ object Clue {
   /**
    * Retrieves clues for the provided column and board.
    * @param col the requested column number
-   * @return Some(Clue) if col is a valid column number, None otherw ise
+   * @return Some(Clue) if col is a valid column number, None otherwise
    */
-  def getClueForCol(col: Int)(using Board): Option[Clue] = ???
+  def getClueForCol(col: Int)(using board: Board): Option[Clue] =
+    Board.getCol(col).map(getClueForList)
 
   /**
    * Retrieves clues for the provided row and board.
    * @param row the requested column number
-   * @return Some(Clue) if row is a valid column number, None otherw ise
+   * @return Some(Clue) if row is a valid column number, None otherwise
    */
-  def getClueForRow(row: Int)(using Board): Option[Clue] = ???
+  def getClueForRow(row: Int)(using Board): Option[Clue] =
+    Board.getRow(row).map(getClueForList)
 
-  private def getClueForList(tiles: List[Tile]): Clue = ???
+  private def getClueForList(tiles: List[Tile]): Clue = {
+    @tailrec
+    def buildClues(tiles: List[Tile], streak: Int, acc: List[Int]): List[Int] = tiles match
+      case Nil if streak > 0 => streak :: acc
+      case Nil => acc
+      case tile :: rest if Board.colored(tile) => buildClues(rest, streak+1, acc)
+      case tile :: rest if !Board.colored(tile) && streak > 0 => buildClues(rest, 0, streak :: acc)
+      case _ :: rest => buildClues(rest, streak, acc)
+    Clue(buildClues(tiles, 0, List.empty[Int]).reverse)
+  }
 }
---
+++ src/test/scala/ClueSpec.scala
@@ -0,0 +1,129 @@
+import BoardSpec.*
+import ClueSpec.exampleBoards
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Prop.passed
+import org.scalatest.FutureOutcome.failed
+import org.scalatest.OptionValues
+import org.scalatest.prop.TableDrivenPropertyChecks.*
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import picross.Board.{Board, Tile}
+import picross.{Board, Clue}
+
+class ClueSpec extends AnyPropSpec with ScalaCheckPropertyChecks {
+  property("clues should pass the table tests for getClueForRow") {
+    forAll(exampleBoards) {
+      (tiles, rows, _) =>
+        Board.newBoard(tiles.map(_.map(Board.newTile(_)))) match
+          case None => failed()
+          case Some(board) =>
+            assert(rows.zipWithIndex.forall(
+              (row, index) => Clue.getClueForRow(index)(using board) match {
+                case None => false
+                case Some(Clue(clues)) => clues == row
+              }))
+    }
+  }
+
+  property("clues should pass the table tests for getClueForCol") {
+    forAll(exampleBoards) {
+      (tiles, _, cols) =>
+        Board.newBoard(tiles.map(_.map(Board.newTile(_)))) match
+          case None => failed()
+          case Some(board) =>
+            assert(cols.zipWithIndex.forall(
+              (col, index) => Clue.getClueForCol(index)(using board) match {
+                case None => false
+                case Some(Clue(clues)) => clues == col
+              }))
+    }
+  }
+}
+
+object ClueSpec {
+  val exampleBoards = Table(
+    ("colors", "expected rows", "expected cols"),
+    (List(
+      List(true, true, false, true),
+      List(true, true, true, true),
+      List(false, false, false, true),
+      List(false, true, false, true)
+    ),
+      List(
+        List(2, 1),
+        List(4),
+        List(1),
+        List(1, 1)),
+      List(
+        List(2),
+        List(2, 1),
+        List(1),
+        List(4)
+      )
+    ),
+    (
+      List(
+        List(false)
+      ),
+      List(List.empty[Int]),
+      List(List.empty[Int])
+    ),
+
+    (
+      List(
+        List(true)
+      ),
+      List(
+        List(1)
+      ),
+      List(
+        List(1)
+      )
+    ),
+
+    (
+      List(
+        List(true, true, true, true, true),
+        List(true, false, false, false, true),
+        List(true, false, false, false, true),
+        List(true, false, false, false, true),
+        List(true, true, true, true, true)),
+      List(
+        List(5),
+        List(1, 1),
+        List(1, 1),
+        List(1, 1),
+        List(5)
+      ),
+      List(
+        List(5),
+        List(1, 1),
+        List(1, 1),
+        List(1, 1),
+        List(5)
+      )
+    ),
+
+    (
+      List(
+        List(true, false, true, false),
+        List(false, false, false, false),
+        List(true, false, true, false),
+        List(false, false, false, false)
+      ),
+      List(
+        List(1, 1),
+        List.empty[Int],
+        List(1, 1),
+        List.empty[Int]
+      ),
+      List(
+        List(1, 1),
+        List.empty[Int],
+        List(1, 1),
+        List.empty[Int]
+      )
+    ),
+  )
+}

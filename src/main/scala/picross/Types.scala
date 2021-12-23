546a801d173dbe3e94c351c30744b9ce59f21827
Kevin Hui <khui.cs@gmail.com>
2021-12-23 16:29:24 -0500

    add various types

---
+++ src/main/scala/picross/Types.scala
@@ -0,0 +1,16 @@
+package picross
+
+/**
+ * Datatypes representing the operations on a Picross board.
+ */
+
+enum BoardMove:
+  // Mark a tile as colored
+  case TileColor(posn: Posn)
+  // Mark a tile as cleared. Automated suggests that the move was
+  // automatically applied after all clues in the clue section were crossed off
+  case TileCross(posn: Posn, automated: Boolean)
+
+// rowClue determines whether the cross is applied to a row
+// or a column, and the index represents the ith row/col.
+case class ClueCross(rowClue: Boolean, index: Int)
